375b9fa2a64cc8604304974bb16ed93b476860c9
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-26 15:02:41 +0100

    Conf.java

---
+++ src/jvm/storm/cookbook/tfidf/Conf.java
@@ -0,0 +1,10 @@
+package storm.cookbook.tfidf;
+
+/**
+ * User: domenicosolazzo
+ */
+public class Conf {
+    public static final String REDIS_HOST_KEY = "redisHost";
+    public static final String REDIS_PORT_KEY = "redisPort";
+    public static final String DEFAULT_JEDIS_PORT = "6379";
+}
