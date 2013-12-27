477670ada28f8dbaa2b19382b3a4f77dc3459597
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-27 14:22:31 +0100

    Adding topology fields

---
+++ src/jvm/storm/cookbook/tfidf/TfidfTopologyFields.java
@@ -0,0 +1,16 @@
+package storm.cookbook.tfidf;
+
+/**
+ * User: domenicosolazzo
+ */
+public class TfidfTopologyFields {
+    public static final String TWEET_TEXT = "text";
+    public static final String TWEET_ID = "id";
+    public static final String TWEET_URLS = "urls";
+    public static final String TWEET_HAS_TAGS = "hasTags";
+
+    public static final String DOCUMENT = "document";
+    public static final String DOCUMENT_ID = "documentId";
+    public static final String TERM = "term";
+    public static final String DIRTY_TERM = "dirtyTerm";
+}
