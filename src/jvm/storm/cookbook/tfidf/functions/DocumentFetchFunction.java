26f000389e7a2f0fe2061537c287da90e59d9fda
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-28 20:12:34 +0100

    Added DocumentFetch functfunction(trident)

---
+++ src/jvm/storm/cookbook/tfidf/functions/DocumentFetchFunction.java
@@ -0,0 +1,15 @@
+package storm.cookbook.tfidf.functions;
+
+import storm.trident.operation.BaseFunction;
+import storm.trident.operation.TridentCollector;
+import storm.trident.tuple.TridentTuple;
+
+/**
+ * User: domenicosolazzo
+ */
+public class DocumentFetchFunction extends BaseFunction {
+    @Override
+    public void execute(TridentTuple objects, TridentCollector tridentCollector) {
+
+    }
+}
